938a32dd071ae4d768f8702bdc870ead7803313c
Spikefire63 <klitschmark@gmail.com>
2024-12-04 21:05:29 -0500

    Game Ending
    added the win and lose to the game.
    the lose is if you drift off to the outer boundaries.
    the win is placed at the top of the asteroid field.
    audio is commented out and needs some additional code once related files are added

---
+++ Assets/Scripts/Ending.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ending : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup WinBackgroundImageCanvasGroup;
+    //public AudioSource WinAudio;
+    public CanvasGroup LoseBackgroundImageCanvasGroup;
+    //public AudioSource LoseAudio;
+
+    bool m_IsPlayerAtEscapePod;
+    bool m_IsPlayerDriftedOff;
+    float m_timer;
+    //bool m_HasAudioPlayed;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            m_IsPlayerAtEscapePod = true;
+        }
+    }
+
+    public void LostPlayer()
+    {
+        m_IsPlayerDriftedOff = true;
+    }
+
+    void Update()
+    {
+        if (m_IsPlayerAtEscapePod)
+        {
+            EndLevel(WinBackgroundImageCanvasGroup, false);
+        }
+        else if (m_IsPlayerDriftedOff)
+        {
+            EndLevel(LoseBackgroundImageCanvasGroup, true);
+        }
+    }
+
+    void EndLevel(CanvasGroup imageCanvaseGroup, bool doRestart)
+    {
+       
+        m_timer += Time.deltaTime;
+        imageCanvaseGroup.alpha = m_timer / fadeDuration;
+
+        if (m_timer > fadeDuration + displayImageDuration)
+        {
+            if (doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+}
